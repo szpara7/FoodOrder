13f6bfd21a22ad93be8ec09499a0625dfbd62840
PS\Przemek <przemek.szpara@gmail.com>
2017-06-30 15:30:09 +0200

    add NinjectControllerFactory

--- FoodOrder/Global.asax.cs
+++ FoodOrder/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodOrder.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,8 @@ namespace FoodOrder
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
