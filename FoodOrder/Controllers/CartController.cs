1b0c58424b9bb4dddda46e499b44a4c123ee4e2f
PS\Przemek <przemek.szpara@gmail.com>
2017-08-11 10:54:13 +0200

    add CartViewModel, add ShowPartialCart

--- FoodOrder/Controllers/CartController.cs
+++ FoodOrder/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using FoodOrder.DAL;
 using FoodOrder.Interfaces.Abstract;
 using FoodOrder.Models;
+using FoodOrder.ViewModel.Cart;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,15 +23,57 @@ namespace FoodOrder.Controllers
         // GET: Cart
         public ActionResult ShowCart()
         {
-            return View(GetCart());
+            var model = new CartViewModel();
+            var data = GetCart().Lines;
+
+            foreach (var i in data)
+            {
+                model.CartLine.Add(new CartLineViewModel()
+                {
+                    Price = i.Price,
+                    ProductName = i.Product.ProductName,
+                    Quantity = i.Quantity
+                });
+            }
+            model.TotalValue = GetCart().TotalValue();
+
+            if (model.CartLine.Capacity == 0)
+            {
+                ViewBag.CartCount = "Cart empty";
+            }
+
+            return View(model);
         }
 
+        public ActionResult ShowPartialCart()
+        {
+            var model = new CartViewModel();
+            List<CartLine> data = GetCart().Lines;
+            
+            foreach(var i in data)
+            {
+                model.CartLine.Add(new CartLineViewModel()
+                {
+                    Price = i.Price,
+                    ProductName = i.Product.ProductName,
+                    Quantity = i.Quantity
+                });
+            }
+            model.TotalValue = GetCart().TotalValue();
+
+            if(!model.CartLine.Any())
+            {
+                ViewBag.CartLineCount = "Cart empty!";
+            }
+
+            return PartialView(model);
+        }
 
         public ActionResult AddToCart(int productId)
         {
             Product product;
             decimal price;
-
+           
             product = productRepository.GetAll()
                 .Where(t => t.ProductID == productId)
                 .FirstOrDefault();
@@ -44,7 +87,7 @@ namespace FoodOrder.Controllers
             {
                 GetCart().AddProduct(product, 1, price);
             }
-
+            
             return RedirectToAction("Index", "Home");  //todo: wracanie do tej strony
         }
 
---
+++ FoodOrder/ViewModel/Cart/CartViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodOrder.ViewModel.Cart
+{
+    public class CartViewModel
+    {
+        public CartViewModel()
+        {
+            CartLine = new List<CartLineViewModel>();
+        }
+        public List<CartLineViewModel> CartLine { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+
+    public class CartLineViewModel
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
