0bcc78769d21a0c161c8f394d286393dd6463cf3
PS\Przemek <przemek.szpara@gmail.com>
2017-09-29 11:42:27 +0200

    add CustomerCRUD

--- FoodOrder/Controllers/CRUD/CustomerCRUDController.cs
+++ FoodOrder/Controllers/CRUD/CustomerCRUDController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using FoodOrder.DAL;
+using FoodOrder.Infrastructure;
+using FoodOrder.Interfaces;
+using FoodOrder.Interfaces.Abstract;
+using FoodOrder.ViewModel.CRUD;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +14,74 @@ namespace FoodOrder.Controllers.CRUD
 {
     public class CustomerCRUDController : Controller
     {
+        private ICustomerRepository customerRepository;
+        private IEmailSender emailSender;
+
+        public CustomerCRUDController(ICustomerRepository customerRepository, IEmailSender emailSender)
+        {
+            this.customerRepository = customerRepository;
+            this.emailSender = emailSender;
+        }
+
         // GET: CustomerCRUD
         public ActionResult Index()
         {
             return PartialView();
+        }
+
+        public ActionResult GetAll()
+        {
+            var model = customerRepository.GetAll()
+                .Select(s => new CustomerCRUDViewModel
+                {
+                    CustomerID = s.CustomerID,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    EMail = s.EMail
+                })
+                .ToList();
+
+            return PartialView(model);
+        }
+
+        public ActionResult GetById(int? customerId)
+        {
+            if (customerId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadGateway);
+            }
+
+            Customer customer = customerRepository.GetById(customerId);
+
+            CustomerCRUDViewModel model = new CustomerCRUDViewModel
+            {
+                CustomerID = customer.CustomerID,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                City = customer.City,
+                CreatedDate = customer.CreatedDate.ToLongDateString(),
+                EMail = customer.EMail,
+                IsConfirmed = customer.IsConfirmed,
+                IsDeleted = customer.IsDeleted,
+                Phone = customer.Phone,
+                PostCode = customer.PostCode,
+                Street = customer.Street
+            };
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        public void SendEmailConfirmation(string userEmail)
+        {
+            string userToken = customerRepository.GetByEmail(userEmail).Token;
+            string userName = customerRepository.GetByEmail(userEmail).FirstName;
+
+            try
+            {
+                emailSender.SendEmail(userEmail, "Confirm email", EmailsBody.ConfirmEmail(userName, userToken, userEmail));
+            }
+            catch(Exception e) {  }
+
         }
     }
 }
---
+++ FoodOrder/ViewModel/CRUD/CustomerCRUDViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodOrder.ViewModel.CRUD
+{
+    public class CustomerCRUDViewModel
+    {
+        public int CustomerID { get; set; }
+
+        [MaxLength(20), Required]
+        public string FirstName { get; set; }
+
+        [MaxLength(30), Required]
+        public string LastName { get; set; }
+
+        [MaxLength(50), Required]
+        public string City { get; set; }
+
+        [MaxLength(50), Required]
+        public string Street { get; set; }
+
+        [Required, StringLength(6)]
+        public string PostCode { get; set; }
+
+        [Required, Phone]
+        public string Phone { get; set; }
+
+        [Required, EmailAddress]
+        public string EMail { get; set; }
+
+        [Required]
+        public string CreatedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool IsConfirmed { get; set; }
+    }
+}
